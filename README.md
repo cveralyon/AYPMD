7c091406a8d0239295c5d16c09b72408c6fda661
Anchor9 <fjmiranda4@miuandes.cl>
2022-06-06 19:30:47 -0400

    testing initial query

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 
 
-### 1) Hay un archivo llamado *"aws_config.py"* que es el que tiene los init donde llamo al bucket y traigo la data, ahi la filtramos y empezamos a trabajar con los graficos y la pagina web
+### 1) Hay un archivo llamado *"aws_config.py"* que es el que tiene los init donde llamo al bucket y traigo la data, ahi la filtramos y empezamos a trabajar con los graficos y la pagina web, por ahora solo me traje el dataframe de "codigo_comuna" hay que traerse los otros y empezar a filtrar
 
 ### 2) Para iniciar el localhost de la pagina web tienes que pararte en AYPMD/aypmd y poner:
     * python manage.py runserver
--- aypmd4/aypmd4/aws_config.py
+++ aypmd4/aypmd4/aws_config.py
@@ -12,7 +12,23 @@ client = boto3.client(
     aws_secret_access_key='FVmBpznSreeuzsyB91uD1tFuUcZ3v5W7GpRQgC7D',
     
 )
-print("pasa222")
+athena = boto3.client('athena', region_name="us-east-1", 
+    aws_access_key_id='AKIATG5KAWOPKFLZDZXK',
+    aws_secret_access_key='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')
+
+athena_job_query = athena.start_query_execution(
+        QueryString='SELECT * from codigo_comuna_region LIMIT 10 ',
+        QueryExecutionContext={
+            'Database': 'grupo4-database'
+        },
+        ResultConfiguration={
+            'OutputLocation': 's3://aypmd-grupo4/athena/'
+        }
+    )   
+query_execution_id = athena_job_query['QueryExecutionId']
+athena_job_status_query = athena.get_query_execution(QueryExecutionId=query_execution_id)
+     
+
 
 s3 = boto3.resource('s3',
                     aws_access_key_id= 'AKIATG5KAWOPKFLZDZXK',
@@ -22,31 +38,41 @@ s3 = boto3.resource('s3',
 #bucket = s3.Bucket('grupo4-aypmd')
 
 
+# -------- CODIGO COMUNA REGION --------------------------
 buffer = io.BytesIO()
-#s3 = boto3.resource('s3')
-#s3_object = s3.Object('bucket-name', 'key/to/parquet/file.gz.parquet')
-#s3_object.download_fileobj(buffer)
-#table = pq.read_table(buffer)
-#df = table.to_pandas()
-
 s3_object = s3.Object('grupo4-aypmd', 'tablas/codigo_comuna_region/codigo_comuna_region.parquet')
 s3_object.download_fileobj(buffer)
 table = pq.read_table(buffer)
-df = table.to_pandas()
-print(df)
-print("METODO")
+df_ccr = table.to_pandas()
+#print(df_ccr)
 
-#my_bucket=s3.Bucket('grupo4-aypmd')
-#bucket_list = []
-#for file in my_bucket.objects.filter(Prefix = 'tablas'):
-#    print(file)
-#    file_name=file.key
-#    if file_name.find(".parquet")!=-1:
-#        file.download_fileobj(buffer)
-#        table = pq.read_table(buffer)
-#        df = table.to_pandas()
-#        print("TERMINA 1 TABLA")
-#        bucket_list.append(file.key)
-#length_bucket_list=print(len(bucket_list))
-#print(bucket_list[0:10])
+# -------- CODIGO DESTINO --------------------------
+buffer = io.BytesIO()
+s3_object = s3.Object('grupo4-aypmd', 'tablas/codigo_destino/codigo_destino.parquet')
+s3_object.download_fileobj(buffer)
+table = pq.read_table(buffer)
+df_cd = table.to_pandas()
+#print(df_cd)
+
+results = athena.get_query_results(QueryExecutionId=query_execution_id)
+print(results)
+for a in results.get("ResultSet").get("Rows"):
+    print(a)
+
+dicc = {}
+i =0;
+for d in results['ResultSet']['Rows']:
+  lista = []
+  for elem in d['Data']:
+      lista.append(elem['VarCharValue'])
+  dicc[i] = lista
+  i+=1
+
+print(dicc)    
+#print(athena_job_query)
+#print(athena_job_status_query['QueryExecution']['Status']['State'])
+
+
+
+
 
