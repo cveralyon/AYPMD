dfbb6bf697b8a4d7663127649193b648348fdf36
Cristian Vera <cavera2@miuandes.cl>
2022-06-07 21:59:11 -0400

    some to deploy heroku

---
+++ aypmd4/aypmd4/graficos.py
@@ -0,0 +1,10 @@
+
+
+
+
+def grafico1(lista1, lista2)
+    plt.plot( lista1,lista1)
+    plt.title('Cantidad de juegos lanzados por año')
+    plt.xlabel('Años')
+    plt.ylabel('Cantidad de juegos')
+    plt.show()
